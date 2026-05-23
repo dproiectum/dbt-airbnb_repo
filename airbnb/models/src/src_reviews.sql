57b47f0fee42445fab4c5ab1a565bdc91ae3efed
dtl <g1t@allops.cloud>
2026-05-23 12:30:46 +0200

    init airbnb project

---
+++ airbnb/models/src/src_reviews.sql
@@ -0,0 +1,12 @@
+WITH raw_reviews as (
+    SELECT * 
+    from AIRBNB.RAW.RAW_REVIEWS
+)
+SELECT
+    LISTING_ID
+    , DATE as review_date
+    , REVIEWER_NAME 
+    , COMMENTS as review_text
+    , SENTIMENT as review_sentiment
+
+FROM raw_reviews
